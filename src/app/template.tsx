19c0b1a730f09864ec38e092149f59cab4cfea91
Oyukawa <kykw.6.swimming@gmail.com>
2024-06-30 12:30:01 +0900

    ローディング実装

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ThemeProvider } from "next-themes";
 import { usePathname } from "next/navigation";
 import { Header } from "@/components/common/Header";
 import { Footer } from "@/components/common/Footer";
 import { NavigationMenu } from "@/components/common/NavigationMenu";
 import { LINK_DATA } from "@/constants/linkData";
+import { Progress } from "@/components/ui/progress";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const currentUrl = usePathname();
@@ -15,6 +16,25 @@ export default function Template({ children }: { children: React.ReactNode }) {
   const [isAction, setIsAction] = useState<boolean>(
     currentPage?.pressShiftKey || false
   );
+  const [progress, setProgress] = useState<number>(0);
+
+  useEffect(() => {
+    const duration = 1000; // 2秒で100にする
+    const increment = 100 / (duration / 1000); // 毎秒の増加量
+    const interval = 100; // 0.1秒ごとに更新
+
+    let currentProgress = 0;
+    const timer = setInterval(() => {
+      currentProgress += increment * (interval / 1000);
+      if (currentProgress >= 100) {
+        currentProgress = 100;
+        clearInterval(timer);
+      }
+      setProgress(currentProgress);
+    }, interval);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <ThemeProvider
       attribute="class"
@@ -29,7 +49,17 @@ export default function Template({ children }: { children: React.ReactNode }) {
             <Header pageName={pageName} isAction={isAction} />
           </div>
         </div>
-        <div className="w-full flex justify-center">{children}</div>
+        <div
+          className="flex justify-center"
+          style={{ display: progress >= 100 ? "none" : "flex" }}
+        >
+          <div className="w-[60%]">
+            <Progress value={progress} />
+          </div>
+        </div>
+        <div style={{ display: progress >= 100 ? "block" : "none" }}>
+          {children}
+        </div>
         <div className="flex justify-end">
           <Footer />
         </div>
