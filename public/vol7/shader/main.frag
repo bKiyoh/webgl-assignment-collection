2b5199fa52fb59f180c7d80c12220412d5e7da9b
Oyukawa <kykw.6.swimming@gmail.com>
2024-09-12 21:36:58 +0900

    一次コミット

--- public/vol7/shader/main.frag
+++ public/vol7/shader/main.frag
@@ -2,10 +2,11 @@ precision mediump float;
 
 uniform sampler2D textureUnit;
 uniform sampler2D textureUnit1;
-uniform vec2 windowSize;
-uniform vec2 textureSize;
+uniform float intensity;
 uniform float progress; 
 varying vec2 vTexCoord;
+
+uniform vec4 resolution;
 
 // 2D回転行列
 mat2 rotate(float angle) {
@@ -14,19 +15,64 @@ mat2 rotate(float angle) {
   return mat2(c, -s, s, c);
 }
 
+const float PI = 3.1415;
+
+float random() { 
+    return fract(sin(2.0 + dot(gl_FragCoord.xy / resolution.xy / 10.0, vec2(12.9898, 4.1414))) * 43758.5453);
+}
+
+float hash(float n) { 
+    return fract(sin(n) * 1e4); 
+}
+
+float hash(vec2 p) { 
+    return fract(1e4 * sin(17.0 * p.x + p.y * 0.1) * (0.1 + abs(sin(p.y * 13.0 + p.x)))); 
+}
+
+float hnoise(vec2 x) {
+  vec2 i = floor(x);
+  vec2 f = fract(x);
+  float a = hash(i);
+  float b = hash(i + vec2(1.0, 0.0));
+  float c = hash(i + vec2(0.0, 1.0));
+  float d = hash(i + vec2(1.0, 1.0));
+  vec2 u = f * f * (3.0 - 2.0 * f);
+  return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
+}
+
 void main() {
   vec2 newUV = vTexCoord;
 
-  // newUV座標を基に、テクスチャを5x1で分割
+  // 1つ目のエフェクト
+  float hn = hnoise(newUV.xy * resolution.xy / 100.0);
+  vec2 d = vec2(0., normalize(vec2(0.5, 0.5) - newUV.xy).y);
+
+  // `mod(progress, 1.0)` を使って `progress` をループさせる
+  float modProgress = mod(progress, 1.0);
+
+  vec2 uv1 = newUV + d * modProgress / 5.0 * (1.0 + hn / 2.0);
+  vec2 uv2 = newUV - d * (1.0 - modProgress) / 5.0 * (1.0 + hn / 2.0);
+
+  // 1つ目のエフェクト結果
+  vec4 t1_effect1 = texture2D(textureUnit, uv1);
+  vec4 t2_effect1 = texture2D(textureUnit1, uv2);
+  vec4 effect1 = mix(t1_effect1, t2_effect1, modProgress);
+
+  // 2つ目のエフェクトの入力として、1つ目のエフェクト結果を使用
   vec2 uvDivided = fract(newUV * vec2(50.0, 1.0));
+  
+  // 2つ目のエフェクトで intensity を適用
+  vec2 uvDisplaced1 = newUV + rotate(3.1415926 / 4.0) * uvDivided * modProgress * 0.1 * intensity;
+  vec2 uvDisplaced2 = newUV + rotate(3.1415926 / 4.0) * uvDivided * (1.0 - modProgress) * 0.1 * intensity;
 
-  vec2 uvDisplaced1 = newUV + rotate(3.1415926 / 4.0) * uvDivided * progress * 0.1;
-  vec2 uvDisplaced2 = newUV + rotate(3.1415926 / 4.0) * uvDivided * (1.0 - progress) * 0.1;
+  // 2つ目のエフェクトで、1つ目の結果を基に処理
+  vec4 t1_effect2 = texture2D(textureUnit, uvDisplaced1);
+  vec4 t2_effect2 = texture2D(textureUnit1, uvDisplaced2);
 
-  // 2つのテクスチャを取得
-  vec4 t1 = texture2D(textureUnit, uvDisplaced1);
-  vec4 t2 = texture2D(textureUnit1, uvDisplaced2);
+  vec4 effect2 =  mix(t1_effect2, t2_effect2, modProgress);
 
-  // 2つのテクスチャを進行度に応じてミックス
-  gl_FragColor = mix(t1, t2, progress);
+  // 最終結果をミックスし、順番に実行
+  vec4 finalEffect = mix(effect1, mix(t1_effect2, t2_effect2, modProgress), modProgress);
+
+  gl_FragColor = finalEffect;
 }
--- src/app/vol7/page.js
+++ src/app/vol7/page.js
@@ -3,14 +3,11 @@ import { useEffect, useRef } from "react";
 import { WebGLUtility } from "@/lib/webGl/webgl.js";
 import { Vec2, Vec3, Mat4 } from "@/lib/webGl/math";
 import { WebGLGeometry } from "@/lib/webGl/geometry.js";
-import { WebGLOrbitCamera } from "@/lib/webGl/camera.js";
-import { Pane } from "@/lib/webGl/tweakpane-4.0.3.min.js";
 
 export default function Page() {
   const initializedRef = useRef(false);
   const initAndLoad = async (app) => {
     app.init();
-    app.setupPane();
     await app.load();
     app.setupGeometry();
     app.setupLocation();
@@ -60,9 +57,9 @@ class App {
   isRendering; // レンダリングを行うかどうかのフラグ
   texture; // テクスチャのインスタンス
   texture1; // テクスチャのインスタンス
-  textureVisibility; // テクスチャの可視性
-  isBlending; // ブレンディングを行うかどうかのフラグ @@@
-  globalAlpha; // グローバルなアルファ値 @@@
+  cameraPosition;
+  cameraTarget;
+  upDirection;
 
   constructor(wrapper, width, height) {
     this.wrapper = wrapper;
@@ -70,19 +67,6 @@ class App {
     this.height = height - App.RENDERER_PARAM.rendererRatio;
     this.resize = this.resize.bind(this);
     this.render = this.render.bind(this);
-  }
-
-  /**
-   * ブレンディングを設定する @@@
-   * @param {boolean} flag - 設定する値
-   */
-  setBlending(flag) {
-    const gl = this.gl;
-    if (flag === true) {
-      gl.enable(gl.BLEND);
-    } else {
-      gl.disable(gl.BLEND);
-    }
   }
 
   /**
@@ -111,14 +95,10 @@ class App {
     this.canvas.width = this.width;
     this.canvas.height = this.height;
 
-    // カメラ制御用インスタンスを生成する
-    const cameraOption = {
-      distance: 5.0, // Z 軸上の初期位置までの距離
-      min: 1.0, // カメラが寄れる最小距離
-      max: 10.0, // カメラが離れられる最大距離
-      move: 2.0, // 右ボタンで平行移動する際の速度係数
-    };
-    this.camera = new WebGLOrbitCamera(this.canvas, cameraOption);
+    // カメラを固定にするための設定
+    this.cameraPosition = Vec3.create(0.0, 0.0, 5.0); // Z軸上に5.0の位置にカメラを配置
+    this.cameraTarget = Vec3.create(0.0, 0.0, 0.0); // カメラの向きを原点に向ける
+    this.upDirection = Vec3.create(0.0, 1.0, 0.0); // カメラの上方向をY軸に設定
 
     // 最初に一度リサイズ処理を行っておく
     this.resize();
@@ -128,60 +108,6 @@ class App {
 
     // 深度テストは初期状態で有効
     this.gl.enable(this.gl.DEPTH_TEST);
-
-    // 初期状態ではテクスチャが見えているようにする
-    this.textureVisibility = true;
-
-    // 初期状態ではブレンディングは無効化 @@@
-    this.isBlending = false;
-
-    // 初期状態ではアルファ値は完全な不透明となるよう設定する @@@
-    this.globalAlpha = 1.0;
-  }
-
-  /**
-   * tweakpane の初期化処理
-   */
-  setupPane() {
-    const gl = this.gl;
-    // Tweakpane を使った GUI の設定
-    const pane = new Pane();
-    const parameter = {
-      texture: this.textureVisibility,
-      blending: this.isBlending,
-      alpha: this.globalAlpha,
-    };
-    // テクスチャの表示・非表示
-    pane.addBinding(parameter, "texture").on("change", (v) => {
-      this.textureVisibility = v.value;
-    });
-    // ブレンドを行うかどうか @@@
-    pane.addBinding(parameter, "blending").on("change", (v) => {
-      this.isBlending = v.value;
-      this.setBlending(v.value);
-    });
-    // グローバルなアルファ値 @@@
-    pane
-      .addBinding(parameter, "alpha", {
-        min: 0.0,
-        max: 1.0,
-      })
-      .on("change", (v) => {
-        this.globalAlpha = v.value;
-      });
-
-    // TweakpaneのDOM要素の取得
-    const paneElement = pane.element;
-    // スタイルの適用で位置を調整
-    paneElement.style.position = "absolute"; // 絶対位置に変更
-    paneElement.style.top = "55px"; // 上からの位置
-    paneElement.style.right = "55px"; // 右からの位置
-    // ラベルの幅を調整して二段にならないようにする
-    const labels = paneElement.querySelectorAll(".tp-lblv_l"); // Tweakpaneのラベル要素を取得
-    for (const label of labels) {
-      label.style.width = "auto"; // 幅を自動調整
-      label.style.whiteSpace = "nowrap"; // テキストの折り返しを防止
-    }
   }
 
   /**
@@ -227,7 +153,7 @@ class App {
         );
         // 画像を読み込み、テクスチャを初期化する
         const image = await WebGLUtility.loadImage("/vol7/sample.jpg");
-        const image1 = await WebGLUtility.loadImage("/vol7/sample1.png");
+        const image1 = await WebGLUtility.loadImage("/vol7/sample1.jpg");
         this.texture = WebGLUtility.createTexture(gl, image);
         this.texture1 = WebGLUtility.createTexture(gl, image1);
         // Promsie を解決
@@ -240,10 +166,15 @@ class App {
    * 頂点属性（頂点ジオメトリ）のセットアップを行う
    */
   setupGeometry() {
-    // プレーンジオメトリの情報を取得
-    const size = 2.0;
+    const width = this.canvas.width;
+    const height = this.canvas.height;
+
+    // アスペクト比を維持しながら、ウィンドウのサイズいっぱいにポリゴンを表示
+    const sizeWidth = width / 166;
+    const sizeHeight = height / 153;
+
     const color = [1.0, 1.0, 1.0, 1.0];
-    this.planeGeometry = WebGLGeometry.plane(size, size, color);
+    this.planeGeometry = WebGLGeometry.plane(sizeWidth, sizeHeight, color);
 
     // VBO と IBO を生成する
     this.planeVBO = [
@@ -282,7 +213,6 @@ class App {
       textureUnit: gl.getUniformLocation(this.program, "textureUnit"),
       textureUnit1: gl.getUniformLocation(this.program, "textureUnit1"),
       useTexture: gl.getUniformLocation(this.program, "useTexture"), // テクスチャを使うかどうかのフラグ @@@
-      globalAlpha: gl.getUniformLocation(this.program, "globalAlpha"), // グローバルアルファ @@@
     };
   }
 
@@ -351,7 +281,11 @@ class App {
     const m = Mat4.identity();
 
     // ビュー・プロジェクション座標変換行列
-    const v = this.camera.update();
+    const v = Mat4.lookAt(
+      this.cameraPosition,
+      this.cameraTarget,
+      this.upDirection
+    );
     const fovy = 45;
     const aspect = window.innerWidth / window.innerHeight;
     const near = 0.1;
@@ -369,10 +303,6 @@ class App {
       this.planeIBO
     );
 
-    // サイズを定義
-    let windowSize = Vec2.create(window.innerWidth, window.innerHeight);
-    let textureSize = Vec2.create(1024, 1024);
-
     // プログラムオブジェクトを選択し uniform 変数を更新する
     gl.useProgram(this.program);
 
@@ -381,8 +311,6 @@ class App {
     gl.uniform1f(this.uniformLocation.intensity, 1.0); // ここで 50.0 などの値を設定します
     gl.uniform1f(this.uniformLocation.progress, progressValue); // ここで 0.0 〜 1.0 の値を設定します
     gl.uniform1f(this.uniformLocation.time, nowTime);
-    gl.uniform2fv(this.uniformLocation.windowSize, windowSize); //表示したいサイズ
-    gl.uniform2fv(this.uniformLocation.textureSize, textureSize); // 画像のサイズ
     gl.uniform4f(
       this.uniformLocation.resolution,
       this.width,
@@ -392,33 +320,10 @@ class App {
     );
     gl.uniform1i(this.uniformLocation.textureUnit, 0);
     gl.uniform1i(this.uniformLocation.textureUnit1, 1);
-    gl.uniform1i(this.uniformLocation.useTexture, this.textureVisibility);
-    gl.uniform1f(this.uniformLocation.globalAlpha, this.globalAlpha); // グローバルアルファ @@@
-
     // １つ目のポリゴンを描画する @@@
     {
       // モデル座標変換行列（１つ目は奥）
-      const m = Mat4.translate(Mat4.identity(), Vec3.create(0.0, 0.0, -0.5));
-      const mvp = Mat4.multiply(vp, m);
-      const normalMatrix = Mat4.transpose(Mat4.inverse(m));
-      gl.uniformMatrix4fv(this.uniformLocation.mvpMatrix, false, mvp);
-      gl.uniformMatrix4fv(
-        this.uniformLocation.normalMatrix,
-        false,
-        normalMatrix
-      );
-      gl.drawElements(
-        gl.TRIANGLES,
-        this.planeGeometry.index.length,
-        gl.UNSIGNED_SHORT,
-        0
-      );
-    }
-
-    // ２つ目のポリゴンを描画する @@@
-    {
-      // モデル座標変換行列（２つ目は手前）
-      const m = Mat4.translate(Mat4.identity(), Vec3.create(0.0, 0.0, 0.5));
+      const m = Mat4.translate(Mat4.identity(), Vec3.create(0.0, 0.0, 0.0));
       const mvp = Mat4.multiply(vp, m);
       const normalMatrix = Mat4.transpose(Mat4.inverse(m));
       gl.uniformMatrix4fv(this.uniformLocation.mvpMatrix, false, mvp);
