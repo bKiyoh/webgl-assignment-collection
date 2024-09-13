4312ba23b34c18fdd9fa0a9a15c49b8c69175c75
Oyukawa <kykw.6.swimming@gmail.com>
2024-09-13 20:45:36 +0900

    リファクタリング

--- src/constants/linkData.ts
+++ src/constants/linkData.ts
@@ -39,9 +39,4 @@ export const LINK_DATA = [
     name: "Vol.7 2024/08/03",
     pressShiftKey: false,
   },
-  {
-    root: "/vol7-2",
-    name: "Vol.7-2 2024/08/03",
-    pressShiftKey: false,
-  },
 ];
